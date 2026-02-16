61ed5ec64b155c9bd588d861d22fda8a3b8bce96
Sammers0821 <sammygrace0821@gmail.com>
2026-02-15 20:48:39 -0600

    Changed App.vue layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="flex min-h-screen flex-col">
+  <div
+    class="bg min-h-screen bg-linear-to-br from-purple-950 via-purple-700 to-purple-300 font-sans"
+  >
     <RouterView />
   </div>
 </template>
